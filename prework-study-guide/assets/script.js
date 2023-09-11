af5ed3853ea874a60622d9c1837a1d52082f87bb
Jose Silva <js38902@gmail.com>
2023-09-10 23:33:48 -0400

    completion of git flow

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,24 +1,31 @@
-var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
+var randomTopic = topics[Math.random() * topics.length];
 
-    console.log(topics[x]);
+function listTopics() {
+ 
+    for (var x = 0; x < topics.length; x++){
+       
+        console.log(topics[x]);
+   
+    }
+
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopics() {
+
+if (randomTopic === 'HTML') {
 
     console.log("Lets study HTML!");
 
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
 
     console.log("Lets study CSS!");
 
-} else if (topic === 'Git') {
-
+} else if (randomTopic === 'Git') {
     console.log("Lets study Git!");
 
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
 
     console.log("Lets study JavaScript!");
 
@@ -26,4 +33,14 @@ if (topic === 'HTML') {
 
     console.log('Please try again!');
 }
-*/
+
+}
+
+console.log('Here are the topics we learned through Prework:');
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopics();
+
